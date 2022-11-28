17ecba50e704321d423c2ac1fb4a61248597d1ea
Väinö Ruhanen <vaino.ruhanen@gmail.com>
2022-11-28 12:29:15 +0200

    sovellus internettiin

---
+++ README.md
@@ -0,0 +1,1 @@
+Internet sovellus: https://sovellus.onrender.com
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('info', function(req, res) {
     const body = JSON.stringify(req.body)
